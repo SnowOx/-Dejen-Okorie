5fce4043c4bed05d7a1a7cde02d6a5c7b64898fd
SnowOx <1239@protonmail.com>
2018-04-11 22:03:23 +0100

    Update - Success
    Success!

--- madLibs2.py
+++ madLibs2.py
@@ -2,16 +2,20 @@
 # MadLibs2.py - Programs that reads in text files and lets the user add their own text
 # Could alter this program to create a method that separates each character (rather than each word as .split() does) in any string
 
-#filename = input('Please enter the filename with its extension: ')
-filename = 'madLibsInput.txt' # For testing
+import datetime
+
+#filename = input('Please enter the filename, with its extension, \nfor the program to read: ')
+
+filename = 'madLibsInput.txt'  # For testing
 openFile = open(filename, 'r')
 readFile = openFile.read()
-print('The current file contains the text:\n ' + readFile + '\n\n')
-writeFile = open('aNewDocument.txt', 'w')
+print('\nThe current file contains the text:\n\n ' + readFile + '\n')
 
-splitText1 = readFile.split()
+# New File
+writeFile = open('madLibsNewDocument%s.txt', 'w')
 
 # Full Stop Separator -- iterate over the first split text and separate strings that have a '.' or ',' as the last character
+splitText1 = readFile.split()
 punctuation_list = ['.'] 
 splitText2 = []
 for word in splitText1:
@@ -22,6 +26,7 @@ for word in splitText1:
         splitText2.append(word[:-1])
         splitText2.append(word[-1])
 
+print(splitText2)
 # Word Replacer -- iterate over the list of separated words and punctutation and ask for input where element appears
 elementsList = ['ADVERB','VERB','NOUN','ADJECTIVE']
 
@@ -39,15 +44,18 @@ for i in range(len(splitText2)):
         else:
             pass
         
-# Remove the floating punctuation in the splitText2 list
-for entry in splitText2:
-    if entry in punctuation_list:
-        print('Deleting ' + entry)
-        del entry
-
-
+# Floating Punctuation Remover -- remove the floating punctuation in the splitText2 list
+for item in punctuation_list:
+    for entry in splitText2:
+        try:
+            splitText2.remove(item)
+            print('Deleted this floating punction mark >> ' + item)
+        except:
+            pass
+        
+# Join the refined string items
 print(' '.join(splitText2))
 writeFile.write(' '.join(splitText2))
 writeFile.close()   
     
-# Incomplete. Program currently overwrites full stops that are adjacent to the detected elements.
+# Success! However, the code seems verbose.
