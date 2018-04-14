5c8f44ca1392786e790593e6acf2080178b838c8
SnowOx <1239@protonmail.com>
2018-04-14 11:49:55 +0100

    Update regexSearch.pu.py
    Solved.

--- regexSearch.pu.py
+++ regexSearch.pu.py
@@ -4,20 +4,26 @@
 import os
 import re
 
-DEBUG = True
-
 # Search folder
-folder_path = 'C:\Test'
-folder_name = os.path.abspath(folder_path)
-regex_input = input('Enter regex')
-user_regex = re.compile(regex_input)
-results_dict = {} # to capture file_name and results
-for file_name in folder_name: 
-    if file_name.endswith('.txt'):
-        results = user_regex.findall(r'%s') % regex_input
-        results_dict[file_name] = results
-    else:
-        pass
+folder_path = input('Enter folder path to search for regex ')
+folder_contents = os.listdir(folder_path)
+user_regex = input(r'Enter regex ')
 
-while DEBUG == True:
-    print(results_dict)
+results_dict = {}
+def store_and_show_results():
+    print('File %s is a .txt file. The program found \'%s\' %s times' %
+          (file_name, user_regex, len(results)))
+    print(results)
+    results_dict[file_name] = results
+    
+for file_name in folder_contents:
+    if file_name.endswith('.txt'):
+        with open(os.path.join(folder_path, file_name), 'r') as input_file:
+            text_to_search = input_file.read()
+        results = re.findall(user_regex, text_to_search)
+        store_and_show_results()   
+    else:
+        print('File %s is not a .txt file' % file_name)
+
+# Solved
+
