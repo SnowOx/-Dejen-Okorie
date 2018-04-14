0c5b33c44864170311f7acebbe8b6bbecc32cd94
SnowOx <1239@protonmail.com>
2018-04-14 13:24:16 +0100

    Add files via upload

---
+++ selective_copy.py
@@ -0,0 +1,43 @@
+#! python3
+# walk_copy_files.py
+
+import os
+import shutil
+import logging
+
+# STARTING_POSITION = input('Enter path from where to begin walk')
+# OUTPUT_POSITION= input('Enter directory to copy files to')
+STARTING_POSITION = r'C:\Test'
+OUTPUT_POSITION = r'C:\Users\X\Desktop\OutputFolder'
+logging.basicConfig(level=logging.DEBUG)
+ 
+def file_copier(file_name, file_source, file_destination):
+    if file_name.endswith('.pdf') or file_name.endswith('.jpg'):
+        shutil.copy(file_source, file_destination)
+        print('Copied %s to %s' % file_name, file_destination)
+    else:
+        pass
+
+def file_locations(file_name):
+    file_name_path = os.path.abspath(file_name)
+    file_name_destination = os.path.join(OUTPUT_POSITION, file_name)
+    print(file_name_path, file_name_destination)
+    return file_name_path
+    return file_name_destination
+
+for root, dirs, files in os.walk(STARTING_POSITION):
+    for file_name in root:
+        file_locations(file_name)
+        file_copier(file_name, file_name_path, file_name_destination)
+    for file_name in dirs:
+        file_locations(file_name)
+        file_copier(file_name, file_name_path, file_name_destination)
+    for file_name in files:
+        file_locations(file_name)
+        file_copier(file_name, file_name_path, file_name_destination)
+        
+print('Walk complete')
+        
+
+
+    
