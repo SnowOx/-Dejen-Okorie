0ccd332edfe76c5f6919f1e8608cbd4d44d61931
SnowOx <1239@protonmail.com>
2018-04-19 23:15:27 +0100

    Update string_stripper.py

--- string_stripper.py
+++ string_stripper.py
@@ -3,18 +3,23 @@
 
 import re
 
+def remove_whitespace(input_string):
+    no_white_regex = re.compile(r'\w')
+    stripped_string = no_white_regex.findall(input_string)
+    output_string = ''.join(stripped_string)
+    return output_string
+
+def remove_user_defined_characters(input_string, user_characters):
+    user_regex = re.compile(r'(?!%s)' % user_characters)
+    stripped_string = user_regex.findall(input_string)
+    output_string = ''.join(stripped_string)
+    return output_string
+    
 def string_strip(input_string, *user_characters):
     if not user_characters:
-        no_white_regex = re.compile(r'\w')
-        stripped_string = no_white_regex.findall(input_string)
-        output_string = ''.join(stripped_string)
-        return output_string
+        remove_whitespace(input_string)
+        
     if user_characters:
-        user_regex = re.compile(r'(%s)' % user_characters)
-        stripped_string = user_regex.findall(input_string)
-        print(stripped_string)
-        output_string = ''.join(stripped_string)
-        print(output_string)
-        return output_string
-
+        remove_user_defined_characters(input_string, user_characters)
+        
 string_strip('The cat hat cat 24332', 'at')
