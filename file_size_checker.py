868c50f1a30df5df01d3e28c767055cdaad81455
SnowOx <1239@protonmail.com>
2018-04-25 12:52:39 +0000

    *To run to check*
    Note that string formatting characters often cause problems!

--- file_size_checker.py
+++ file_size_checker.py
@@ -5,16 +5,15 @@ import os
 starting_position = "C:\\"
 
 def show_big_file_details(file):
-    get_file_details(file)
-    print('%s -- %s MB') % (file_path, file_size)
+    file_path, file_size = get_file_details(file)
+    print('%s = %s MB' % (file_path, file_size/1000000))
 
-    
-def  get_file_details(file):
+def get_file_details(file):
     file_path = os.path.join(starting_position, file)
     print(file_path)
     file_size = os.path.getsize(file_path)
     print(file_size)
-    if file_size > 100000000:
+    if file_size > 10000000:
         return(file_path, file_size)
 
 for root, dirs, files in os.walk(starting_position):
