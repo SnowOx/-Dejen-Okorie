eba1b471ad46a5136e45d19b1c9174742a33a009
SnowOx <1239@protonmail.com>
2018-04-25 23:15:44 +0100

    Update file_size_checker.py

--- file_size_checker.py
+++ file_size_checker.py
@@ -2,24 +2,15 @@
 # file_size_searcher
 
 import os
-starting_position = "C:\\"
+starting_position = "c:\\Users\\A\\Google Drive\\521Y Graphics"
 
-def show_big_file_details(file):
-    file_path, file_size = get_file_details(file)
-    print('%s = %s MB' % (file_path, file_size/1000000))
+for dirpath, dirnames, filenames in os.walk(starting_position):
+    for filename in filenames:
+        file_path = os.path.join(dirpath, filename)
+        file_size = os.path.getsize(file_path)
+        if file_size > 1000000: 
+            print('%s = %s MB' % (file_path, file_size/1000000))
+    
 
-def get_file_details(file):
-    file_path = os.path.join(starting_position, file)
-    print(file_path)
-    file_size = os.path.getsize(file_path)
-    print(file_size)
-    if file_size > 10000000:
-        return(file_path, file_size)
 
-for root, dirs, files in os.walk(starting_position):
-    for file in root:
-        show_big_file_details(file)
-    for file in dirs:
-        show_big_file_details(file)
-    for file in files:
-        show_big_file_details(file)
+        
