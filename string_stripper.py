b0d3fbcc6c73b0e7bbf1ca0ba2aeb282f334838f
SnowOx <1239@protonmail.com>
2018-04-27 21:47:17 +0100

    Update string_stripper.py

--- string_stripper.py
+++ string_stripper.py
@@ -10,16 +10,16 @@ def remove_whitespace(input_string):
     return output_string
 
 def remove_user_defined_characters(input_string, user_characters):
-    user_regex = re.compile(r'(?!%s)' % user_characters)
+    print(user_characters)
+    user_regex = re.compile(rf'^((?!{user_characters[0]}).)*$')
     stripped_string = user_regex.findall(input_string)
+    print(stripped_string)
     output_string = ''.join(stripped_string)
     return output_string
     
 def string_strip(input_string, *user_characters):
     if not user_characters:
-        remove_whitespace(input_string)
+        return remove_whitespace(input_string)   
+    return remove_user_defined_characters(input_string, user_characters)
         
-    if user_characters:
-        remove_user_defined_characters(input_string, user_characters)
-        
-string_strip('The cat hat cat 24332', 'at')
+print(string_strip('The tiki taka cat hat cat 24332', 'at'))
