77eec87ad02fe2e25d412752defe58da3e1b5de5
SnowOx <1239@protonmail.com>
2018-04-28 15:39:25 +0100

    Solved

--- selective_filetype_copier.py
+++ selective_filetype_copier.py
@@ -4,28 +4,29 @@
 import os
 import shutil
 
-input_position = os.path.abspath('C:\\Users\\A\\Downloads')
+input_position = os.path.abspath('C:\\Users\\A\\Desktop\\TestFolder')
 output_position = os.path.abspath('C:\\Users\\A\\Desktop\\OutputFolder')
  
+def get_file_paths(file_name, input_position, output_position):
+    file_name_source = os.path.join(input_position, file_name)
+    file_name_destination = os.path.join(output_position, file_name)
+    return (file_name_source, file_name_destination)
+
 def return_only_desired_file_types(file_name):
     if file_name.endswith('.pdf') or file_name.endswith('.jpg'):
         return file_name
-    
+
 def file_copier(file_name, file_source, file_destination):
-        shutil.copy(file_source, file_destination)
-        print('Copied %s from %s >>> %s' % file_name, file_source, file_destination)
+    shutil.copy(file_source, file_destination)
+    print('\nCopied %s \nfrom %s \nto %s' % (file_name, file_source, file_destination))
 
-def get_file_paths(file_name, input_position, output_position):
-    file_name_source = os.path.join(file_name, input_position_
-    file_name_destination = os.path.join(file_name, output_position)
-    return (file_name_source, file_name_destination)
+for dirpath, dirnames, filenames in os.walk(input_position):
+    for file_name in filenames:
+        file_name_source, file_name_destination = get_file_paths(file_name, input_position, output_position)
+        desired_file_name = return_only_desired_file_types(file_name)
+        if desired_file_name:       
+            file_copier(desired_file_name, file_name_source, file_name_destination)
+        
+print('\nDone')
 
-for dirpath, dirnames, filenames in os.walk(starting_position):
-    for filename in filenames:
-        get_file_paths(file_name, input_position, output_position) = file_name_source, file_name_destination
-        return_only_desired_file_types(file_name) = file_name
-        if file_name:              
-            file_copier(file_name, file_name_source, file_name_destination)
-        
-print('Done')
-        
+# Solved!
