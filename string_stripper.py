0e4d83b197e9d221a8ae1795c95b4e110371d199
SnowOx <1239@protonmail.com>
2018-05-02 22:55:02 +0100

    Solved!

--- string_stripper.py
+++ string_stripper.py
@@ -1,5 +1,5 @@
-#! python3
-# string_stripper.py
+#!python3
+# strip_string.py
 
 import re
 
@@ -10,16 +10,14 @@ def remove_whitespace(input_string):
     return output_string
 
 def remove_user_defined_characters(input_string, user_characters):
-    print(user_characters)
-    user_regex = re.compile(rf'^((?!{user_characters[0]}).)*$')
-    stripped_string = user_regex.findall(input_string)
-    print(stripped_string)
+    user_regex = re.compile('%s' % user_characters)
+    stripped_string = user_regex.sub('', input_string)
     output_string = ''.join(stripped_string)
     return output_string
-    
-def string_strip(input_string, *user_characters):
+
+def strip_string(input_string, *user_characters):
     if not user_characters:
-        return remove_whitespace(input_string)   
+        return remove_whitespace(input_string)
     return remove_user_defined_characters(input_string, user_characters)
-        
-print(string_strip('The tiki taka cat hat cat 24332', 'at'))
+
+# Solved!
