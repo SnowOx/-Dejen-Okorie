bac262375719875a45e063772d8cb6acf89b7677
SnowOx <1239@protonmail.com>
2018-06-09 23:43:00 +0000

    Update hangman_progress.py

--- hangman_progress.py
+++ hangman_progress.py
@@ -5,16 +5,18 @@
 # # Would like to create a GUI after I have completed the basic mechanics
 """
   _ _ _ _
-  |     |  
-  |   \ O / 
-  |     | 
+  |     |
+  |   \ O /
+  |     |
   |    / \
- _^_ _  
+ _^_ _
 """
 
 import random
+#import requests
+#import bs4
 
-TEXT_FILE_LOCATION = "english_words.txt"
+TEXT_FILE_LOCATION = "E:\\Google Drive\\OP2\\dk\\english_words.txt"
 
 def pick_random_hidden_word(english_words):
     hidden_word = random.choice(english_words)
@@ -42,18 +44,32 @@ def print_hidden_letter_and_marker_list(hidden_letter_and_marker_list):
                 print(' ' + str(k) + ' ', end='')
             if v is 0:
                 print(' ' + '*' + ' ', end='')
+    print()
 
-                
+
 def get_letter_guess(letter_guess_record):
-    letter_guess = input('Enter a letter to guess >> ').upper()
-    if letter_guess in letter_guess_record:
+    letter_guess = input('Enter a letter to guess, or enter * for a clue>> ').upper()
+    if letter_guess == '*':
+        show_clue(hidden_word)
+        get_letter_guess(letter_guess_record)
+    elif letter_guess in letter_guess_record:
         print('You have already guessed this letter. Please guess again!')
-        get_letter_guess(letter_guess_record)    
+        get_letter_guess(letter_guess_record)
     return letter_guess
 
 
+def show_clue(hidden_word):
+    clue_page_request = requests.get(
+        'https://www.wordwebonline.com/search.pl?w=' + hidden_word)
+    clue_page_request.raise_for_status()
+    clue_page = bs4.BeautifulSoup(clue_page_request)
+    clue_page_elements = clue_page.select('span [class="def"]')
+    clue = clue_page_elements[0].getText()
+    print('The clue is:\n%s' % clue)
+
+
 def create_letter_guess_records():
-    letter_guesses_record = []
+    letter_guess_record = []
     number_of_guesses_record = 0
     return (letter_guess_record, number_of_guesses_record)
 
@@ -62,17 +78,32 @@ def update_letter_guess_records(letter_guess, letter_guess_record, number_of_gue
     letter_guess_record.append(letter_guess)
     number_of_guesses_record += 1
     return (letter_guess_record, number_of_guesses_record)
-    
-    
+
+hangman_image =
+['  _ _ _ _  '],
+[' |     |   '],
+[' |   \ O / '],
+[' |     |   '],
+[' |    / \  '],
+[' _^_ _     '],
+
+
 def check_guess_and_update(letter_guess, hidden_letter_and_marker_list):
+    correct_or_incorrect_guess_counter = 0
     for letter_and_marker_pair in hidden_letter_and_marker_list:
         for k in letter_and_marker_pair.keys():
             if k == letter_guess:
                 letter_and_marker_pair[k] = 1
-                print('DEBUG: Correct guess')
-            elif k != letter_guess:
-                print('Incorrect guess')
- 
+                correct_or_incorrect_guess_counter += 1
+    show_correct_or_incorrect_message_for_the_guess(correct_or_incorrect_guess_counter, letter_guess)
+
+
+def show_correct_or_incorrect_message_for_the_guess(correct_or_incorrect_guess_counter, letter_guess):
+    if correct_or_incorrect_guess_counter > 0:
+        print('Correct! The letter %s appears in the word' % letter_guess)
+    else:
+        print('Incorrect! The letter %s does not appear in the word' % letter_guess)
+
 
 def check_whether_game_complete(hidden_letter_and_marker_list, hidden_word_length):
     game_progress_counter = 0
@@ -88,10 +119,10 @@ def check_whether_game_complete(hidden_letter_and_marker_list, hidden_word_lengt
 
 
 def show_end_of_game_congratulations(hidden_word):
-    print('\nCongratulations! You guessed the hidden word %s' % hidden_word)
+    print('\nCongratulations! You guessed the hidden word \'%s\'!' % hidden_word.title())
 
-    
-def main_game_loop(is_game_finished, hidden_letter_and_marker_list,hidden_word_length):
+
+def main_game_loop(is_game_finished, hidden_letter_and_marker_list, hidden_word_length):
     letter_guess_record, number_of_guesses_record = create_letter_guess_records()
     while is_game_finished != True:
         letter_guess = get_letter_guess(letter_guess_record)
@@ -99,19 +130,19 @@ def main_game_loop(is_game_finished, hidden_letter_and_marker_list,hidden_word_l
         check_guess_and_update(letter_guess, hidden_letter_and_marker_list)
         print_hidden_letter_and_marker_list(hidden_letter_and_marker_list)
         is_game_finished = check_whether_game_complete(hidden_letter_and_marker_list, hidden_word_length)
-     
-    
+
+
 def play_hangman_game():
     hidden_word = pick_random_hidden_word(english_words)
-    print(hidden_word) # DBUG To remove
+    print(hidden_word)  # DBUG To remove
     hidden_word_length = get_hidden_word_length(hidden_word)
     hidden_letter_and_marker_list = generate_hidden_letters_and_markers_for(hidden_word)
     is_game_finished = False
     main_game_loop(is_game_finished, hidden_letter_and_marker_list, hidden_word_length)
     show_end_of_game_congratulations(hidden_word)
-    
-    
+
+
 # Engine
 with open(TEXT_FILE_LOCATION) as open_file:
-    english_words = open_file.read().splitlines() # capitalise english_words ?
+    english_words = open_file.read().splitlines()  # capitalise english_words ?
 play_hangman_game()
