d1411ed6d8d763be6f43b5b80194058fe370fafe
SnowOx <1239@protonmail.com>
2018-06-20 23:08:39 +0100

    Solved!

--- cmd_line_emailer.py
+++ cmd_line_emailer.py
@@ -5,27 +5,27 @@ import time
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import getpass
 
-WEBPAGE = 'https://mail.protonmail.com/login'
 
 def get_browser_object():
     browser_object = webdriver.Firefox()
     return browser_object
 
+
 def load_webpage(browser_object):
     browser_object.get(WEBPAGE)
+
 
 def add_time_delay(seconds):
     time.sleep(int(seconds))
 
+
 def get_login_input():
-    username = '1239'
-    #password_1 = input('Enter password 1: ')
-    password_1 = 'abciabci..'
-    #password_2 = input('Enter password 2: ')
-    password_2 = '..,,..,,'
+    username = # Enter protonmail username
+    password_1 = sys.argv[1]
+    password_2 = sys.argv[2]
     return(username, password_1, password_2)
+
 
 def enter_login_information(browser_object, username, password_1, password_2):
     username_field = browser_object.find_element_by_css_selector('#username')
@@ -43,16 +43,18 @@ def enter_login_information(browser_object, username, password_1, password_2):
     except:
         raise ValueError('The password_2 login failed')
 
-def get_recipient_email_address(): # Perhaps add default to test other functtionality
-    recipient_email_address = sys.argv[2]
+
+def get_recipient_email_address():
+    recipient_email_address = sys.argv[3]
     return recipient_email_address
 
+
 def get_message_string():
-    message_string = sys.argv[2:]
+    message_string = sys.argv[4]
     return message_string
 
+
 def open_new_message(browser_object):
-    compose_button = ''
     while True:
         try:
             compose_button = browser_object.find_element_by_css_selector('.compose')
@@ -63,33 +65,53 @@ def open_new_message(browser_object):
     compose_button.click()
 
 
-def write_message(browser_object, recipient_email_address, message_string):
-    while True:
-        try:
-            to_field = browser_object.switch_to_active_element()
-            to_field.send_keys(recipient_email_address)
-            break
+def write_message(browser_object):
+    add_time_delay(2)
+    add_recipient_address(browser_object)
+    add_subject(browser_object)
+    add_body_text(browser_object)
+    send_email(browser_object)
 
-        except:
-            print('Waiting for new message to load')
-            add_time_delay(2)
 
-def click_send_email(browser_object):
-    click_send_email_button = browser_object.find_element_by_css_selector("button.mobileFull:nth-child(4)")
-    click_send_email_button.click()
-    
+def add_recipient_address(browser_object):
+    to_field = browser_object.switch_to_active_element()
+    to_field.send_keys(recipient_email_address, Keys.TAB)
+    to_field.send_keys(Keys.TAB)
 
-#Engine
-    
-#recipient_email_address = get_recipient_email_address(sys.argv) # Perhaps add default to test other functtionality
-recipient_email_address = '000td000@gmail.com'
-#message_string = get_message_string(sys.argv)
+
+def add_subject(browser_object):
+    subject_field = browser_object.switch_to_active_element()
+    subject_field.send_keys(SUBJECT)
+    subject_field.send_keys(Keys.TAB)
+    add_time_delay(2)
+
+
+def add_body_text(browser_object):
+    body_field = browser_object.switch_to_active_element()
+    body_field.send_keys(Keys.CONTROL + "a")
+    body_field.send_keys(Keys.DELETE)
+    body_field.send_keys(BODY_TEXT)
+    body_field.send_keys(Keys.TAB * 6)
+    add_time_delay(1)
+
+
+def send_email(browser_object):
+    send_button = browser_object.switch_to_active_element()
+    send_button.click()
+
+
+# Engine
+
+WEBPAGE = 'https://mail.protonmail.com/login'
+BODY_TEXT = get_message_string()
+recipient_email_address = get_recipient_email_address()
 username, password_1, password_2 = get_login_input()
-message_string = 'test test test 123 test'
+SUBJECT = ('Message from %s!' % username)
 browser_object = get_browser_object()
 load_webpage(browser_object)
 enter_login_information(browser_object, username, password_1, password_2)
 add_time_delay(4)
 open_new_message(browser_object)
-write_message(browser_object, recipient_email_address, message_string)
-click_send_email(browser_object)
+write_message(browser_object)
+
+#Solved!
