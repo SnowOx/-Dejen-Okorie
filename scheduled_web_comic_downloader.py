1d6d59f3d3c4f7bd32d4bdf98da8a0be3489e54a
SnowOx <1239@protonmail.com>
2018-06-26 23:53:39 +0100

    Unsolved

--- scheduled_web_comic_downloader.py
+++ scheduled_web_comic_downloader.py
@@ -2,52 +2,89 @@
 # scheduled_web_comic_downloader.py
 
 import requests
-import bs4
+from bs4 import BeautifulSoup
+import os
+
+# 1. Not writing downloaded url to the database text file
+# 2. Variables are confusingly named. Suggest changing variables to include 'site_url' and 'image_url' among others
+
 
 COMIC_BOOK_URLS = ['http://www.lefthandedtoons.com/', 'https://www.safelyendangered.com/']
+LOCATION_TO_SAVE_COMICS = 'C:\\Users\\A\\Downloads'
 
-record_of_comic_image_urls = # rather than a list, open a text file with a list of the urls
+
+def open_the_record_of_downloaded_comic_images():
+    comic_images_database_path = os.path.join(
+        LOCATION_TO_SAVE_COMICS,'record_of_previously_downloaded_comic_images.txt')
+    try:
+        with open(comic_images_database_path, 'r') as infile:
+            record_of_downloaded_comic_images = infile.read()
+            infile.close()
+    except FileNotFoundError:
+        record_of_downloaded_comic_images = open(comic_images_database_path, 'w')
+        record_of_downloaded_comic_images.close()
+    return record_of_downloaded_comic_images
 
 
-def download_page():
+def download_page(url):
     page_download = requests.get(url)
     page_download.raise_for_status()
     return page_download
 
 
-def get_image_link_from_page():
-    page_soup = bs4.BeautifulSoup(page_download.text, 'lmxl')
-    comic_image_urls = page_soup.select('img class[src]')
-    print(comic_image_urls)
-    return comic_image_urls
+def get_comic_image_url_from_page(page_download, comic_url):
+    page_soup = BeautifulSoup(page_download.text, 'lxml')
+    if comic_url == 'http://www.lefthandedtoons.com/':
+        for tag in page_soup.find_all('img'):
+            attribute_list = tag.get_attribute_list("class")
+            if "comicimage" in attribute_list:
+                comic_image_url = tag["src"]
+    elif comic_url == 'https://www.safelyendangered.com/': #todo: find image url for comic_url with beautiful soup
+        pass
+    return comic_image_url
 
-def open_the_record_of_downloaded_comic_images():
-    with open('record_of_downloaded_comic_images.txt', 'r') as infile:
-        record_of_downloaded_comic_images = infile.read()
+
+def check_if_image_link_has_been_updated(url):
+    if str(url) in record_of_previously_downloaded_comic_images:
+        print('No updates found for %s' % str(url))
+    else:
+        print('%s not present in records. Downloading!' % url)
+        image_request = get_image_file(url)
+        save_image_file(url, image_request)
+        write_downloaded_comic_urls_to_record_of_comic_image_urls(url)
+        print('Downloaded')
+
+
+def write_downloaded_comic_urls_to_record_of_comic_image_urls(url):
+    with open('record_of_previously_downloaded_comic_images.txt', 'w') as infile:
+        record_of_downloaded_comic_images = infile.write(str(url) + '\n')
+        infile.close()
         return record_of_downloaded_comic_images
 
 
-def check_if_image_link_has_been_updated(comic_image_urls):
-    for image_url in comic_image_urls:
-        if image_url in record_of_comic_image_urls:
-            print(image)
-            pass
-        else:
-            # otherwise access
+def get_image_file(image_url):
+    print('image url = %s' % image_url)
+    requests_object = requests.get(image_url)
+    print(type(image_url))
+    requests_object.raise_for_status()
+    downloaded_image_content = requests.get(image_url).content
+    print('downloaded_image_content' + str(type(downloaded_image_content)))
+    return downloaded_image_content
 
 
-
-
-
-def write_downloaded_comic_urls_to_record_of_comic_image_urls()
-
-
+def save_image_file(image_url, downloaded_image):
+    image_filename = str('filename') + '.png'
+    print('Saving %s' % image_filename)
+    save_filename_path = os.path.join(LOCATION_TO_SAVE_COMICS, image_filename)
+    image_file = open(save_filename_path, 'wb')
+    image_file.write(downloaded_image)
+    image_file.close()
 
 
 # Engine
-record_of_downloaded_comic_images = open_the_record_of_downloaded_comic_images()
-for comic in COMIC_BOOK_URLS:
-    page_download = download_page()
-    image_url = get_image_link_from_page()
-    check_if_image_link_has_been_updated(comic_image_urls
-
+record_of_previously_downloaded_comic_images = open_the_record_of_downloaded_comic_images()
+for comic_url in COMIC_BOOK_URLS:
+    print('comic_url = %s' % comic_url)
+    page_download = download_page(comic_url)
+    comic_image_url = get_comic_image_url_from_page(page_download, comic_url)
+    check_if_image_link_has_been_updated(comic_image_url)
