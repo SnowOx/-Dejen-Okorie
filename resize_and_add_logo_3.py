47e37c5414a950b9866bd4d27352125f18342e63
SnowOx <1239@protonmail.com>
2018-07-05 17:11:12 +0100

    *to run before editing*

--- resize_and_add_logo_3.py
+++ resize_and_add_logo_3.py
@@ -8,16 +8,27 @@ from PIL import Image
 SQUARE_FIT_SIZE = 300
 LOGO_FILENAME = 'catlogo.png'
 
-logoIm = Image.open(LOGO_FILENAME)
-logoWidth, logoHeight = logoIm.size
 
-os.makedirs('withLogo', exist_ok=True)
-for filename in os.listdir('.'):
-	filename = filename.lower()
+def save_image_to_file(image, filename):
+    image_new_save_path = os.path.join('processed_image_', filename)
+    image.save(image_new_save_path)
+    print('Saved image to path >> %s' % image_new_save_path) 
+
+    
+# open_image(path) use this as a generic function
+logo_image = Image.open(LOGO_FILENAME)
+logoWidth, logoHeight = logo_image.size  
+# make_new_directory(path)
+os.makedirs('processed_images', exist_ok=True)
+for filename in os.listdir('.'): # add path
+    # check_that_filename_is_an_image_filename
+    filename = filename.lower()
     if not (filename.endswith('.png') or filename.endswith('.jpg') or filename.endswith('.bmp') or filename.endswith('.gif'):
         continue
-    im = Image.open(filename)
-    width, height = im.size
+    # open_image_file()
+    image_file = Image.open(filename)
+    width, height = image_file.size
+    # resize_image_if_dimensions_are_greater_than_square_fit_size
     if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
         if width > height:
             height = int((SQUARE_FIT_SIZE / width) * height)
@@ -27,18 +38,20 @@ for filename in os.listdir('.'):
             height = SQUARE_FIT_SIZE
         print('Resizing %s...' % (filename))
         im = im.resize((width, height))
+    # add_logo_if_image_is_big_enough()
     width, height = im.size
     if width < 2 * logoWidth:
         print("Image width too narrow to add logo")
+        save_image_to_file(im, filename)
         continue
     elif height < 2 * logoHeight:
         print("Image height too short to add logo")
+        save_image_to_file(im, filename)
         continue
     else:
         print('Adding logo to %s...' % (filename))
         im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
-    im.save(os.path.join('processed_image_', filename))
+        save_image_to_file(im, filename)
+print('Done')
 
 
-
-# Procedural style. Edit to improve style
